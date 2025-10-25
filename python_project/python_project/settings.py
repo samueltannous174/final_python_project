b4f12c83d5f02611a8b4d84d5f65da5a996114ba
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-10-26 00:17:26 +0300

    new updates

--- python_project/python_project/settings.py
+++ python_project/python_project/settings.py
@@ -84,8 +84,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'final_python_project',         
+        'USER': 'root',               
+        'PASSWORD': '1201123',  
+        'HOST': '127.0.0.1',          
+        'PORT': '3306',              
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
